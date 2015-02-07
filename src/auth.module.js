c2a8c03a937723d176558777eb6d1d81501849c1
Arnout Aertgeerts <arnoutaertgeerts@gmail.com>
2015-02-07 18:19:45 +0100

    Added pouchdb-authentication methods

--- src/auth.module.js
+++ src/auth.module.js
@@ -4,5 +4,17 @@
     angular
         .module('authorization',  [
             'pouchdb'
-        ]);
+        ])
+        .config(function(pouchDBProvider, POUCHDB_METHODS) {
+            // Add pouchdb authentication methods to angular-pouchdb
+            var authMethods = {
+                login: 'qify',
+                logout: 'qify',
+                getUser: 'qify',
+                getSession: 'qify'
+            };
+
+            pouchDBProvider.methods = angular.extend({}, POUCHDB_METHODS, authMethods);
+        })
+    ;
 })();
